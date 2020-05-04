69bdb4217d3ecafb1a943652b101039922d472ec
maustin1 <mtv1243@gmail.com>
2020-05-04 13:32:19 -0400

    remove lodging section, countdown clock, map in iframe, change date section to explain why we cancelled the event and that we are doing it next year

--- script.js
+++ script.js
@@ -20,15 +20,6 @@ $(".item02").click(()=>{
   );
 });
 
-$(".item03").click(()=>{
-  $("html").animate(
-    {
-      scrollTop: $('.lodging-wrapper').offset().top
-    },
-    "slow"
-  );
-});
-
 $(".scroll-top").click(()=>{
   $("html").animate(
     {
@@ -37,28 +28,3 @@ $(".scroll-top").click(()=>{
     "slow"
   );
 });
-
-/*===============
-    countDown
-================*/
-
-let countDownEl = document.querySelector('.countDown');
-
-let countDownDate = new Date('Sep 19, 2020 16:30:00').getTime();
-
-//calculate the time remaining every second
-let updateCount = setInterval(()=>{
-
-  let now = new Date().getTime();
-
-  let distance = countDownDate - now;
-
-  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  //insert the time into the page
-  countDownEl.innerHTML = days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds
- + 's';
-}, 1000)
