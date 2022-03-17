c49850d63ffcbf9c173cae5faf626690e88a4e71
maustin1 <mtv1243@gmail.com>
2022-03-17 15:40:49 -0400

    Redesign site for new date. Add images in cards for ceremony, location, and lodging. Add embed of Goolge maps hotel location. Update Mailchimp form to remove question about guests doing a small performance.

--- script.js
+++ script.js
@@ -2,10 +2,20 @@
     nav bar
 ===============*/
 
+// nav bar slide toggle
+$(".menu-text").click(() => {
+  $(".nav-wrapper-mobile").slideToggle();
+})
+
+$(".item").click((event) => {
+  $(".nav-wrapper-mobile").slideUp();
+})
+
+// scrolling to elements on click
 $(".item01").click(()=>{
   $("html").animate(
     {
-      scrollTop: $('.date-wrapper').offset().top
+      scrollTop: $('.date-wrapper').offset().top-250
     },
     "slow"
   );
@@ -14,7 +24,25 @@ $(".item01").click(()=>{
 $(".item02").click(()=>{
   $("html").animate(
     {
-      scrollTop: $('#mc_embed_signup').offset().top
+      scrollTop: $('.reception-wrapper').offset().top-200
+    },
+    "slow"
+  );
+});
+
+$(".item03").click(()=>{
+  $("html").animate(
+    {
+      scrollTop: $('.hotel').offset().top-200
+    },
+    "slow"
+  );
+});
+
+$(".item04").click(()=>{
+  $("html").animate(
+    {
+      scrollTop: $('#mc_embed_signup').offset().top-200
     },
     "slow"
   );
